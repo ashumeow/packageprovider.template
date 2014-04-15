ad614f46c8e9c7dbf13640b30866f1f0fe432a3d
Garrett Serack <gserack@gmail.com>
2014-04-15 11:41:56 -0700

    fixed namespaces

--- Class1.cs
+++ Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Template.PackageProvider
+﻿namespace OneGet.PackageProvider.Template
 {
     public class Class1
     {
