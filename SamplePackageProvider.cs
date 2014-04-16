49d6d4c27deca2281d7bb92c5d898909645e235e
Garrett Serack <gserack@gmail.com>
2014-04-16 13:49:13 -0700

    Refactored and switched to a new interface generator

--- SamplePackageProvider.cs
+++ SamplePackageProvider.cs
@@ -1,4 +1,4 @@
-﻿// 
+// 
 //  Copyright (c) Microsoft Corporation. All rights reserved. 
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -13,8 +13,197 @@
 //  
 
 namespace OneGet.PackageProvider.Template {
+    using Utility;
+    using Callback = System.Func<string, System.Collections.Generic.IEnumerable<object>, object>;
+
     public class SamplePackageProvider {
-#region implement packageprovider-interface 
-#endregion
+        #region implement packageprovider-interface
+
+        public void AddPackageSource(string name, string location, bool trusted, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'AddPackageSource'" );
+            }
+
+        }
+        public bool FindPackage(string name, string requiredVersion, string minimumVersion, string maximumVersion, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'FindPackage'" );
+            }
+
+            return  default(bool);
+        }
+        public bool FindPackageByFile(string file, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'FindPackageByFile'" );
+            }
+
+            return  default(bool);
+        }
+        public bool FindPackageByUri(string u, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'FindPackageByUri'" );
+            }
+
+            return  default(bool);
+        }
+        public void GetInstallationOptionDefinitions(Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'GetInstallationOptionDefinitions'" );
+            }
+
+        }
+        public bool GetInstalledPackages(string name, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'GetInstalledPackages'" );
+            }
+
+            return  default(bool);
+        }
+        public void GetMetadataDefinitions(Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'GetMetadataDefinitions'" );
+            }
+
+        }
+
+        /// <summary>
+            /// Returns the name of the Provider. Doesn't need callback .
+            /// </summary>
+            /// <returns></returns>
+        public string GetPackageProviderName(){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+
+            return  default(string);
+        }
+        public bool GetPackageSources(Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'GetPackageSources'" );
+            }
+
+            return  default(bool);
+        }
+        public void Initialize(Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'Initialize'" );
+            }
+
+        }
+        public bool InstallPackageByFastpath(string fastPath, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'InstallPackageByFastpath'" );
+            }
+
+            return  default(bool);
+        }
+        public bool InstallPackageByFile(string filePath, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'InstallPackageByFile'" );
+            }
+
+            return  default(bool);
+        }
+        public bool InstallPackageByUri(string u, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'InstallPackageByUri'" );
+            }
+
+            return  default(bool);
+        }
+        public bool IsTrustedPackageSource(string packageSource, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'IsTrustedPackageSource'" );
+            }
+
+            return  default(bool);
+        }
+        public bool IsValidPackageSource(string packageSource, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'IsValidPackageSource'" );
+            }
+
+            return  default(bool);
+        }
+        public void RemovePackageSource(string name, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'RemovePackageSource'" );
+            }
+
+        }
+        public bool UninstallPackage(string fastPath, Callback c){
+             // TODO: Fill in implementation
+             // Delete this method if you do not need to implement it
+             // Please don't throw an not implemented exception, it's not optimal.
+            using (var request = new Request(c)) {
+                // use the request object to interact with the OneGet core:
+                request.Debug("Information","Calling 'UninstallPackage'" );
+            }
+
+            return  default(bool);
+        }
+
+        #endregion
+
     }
 }
